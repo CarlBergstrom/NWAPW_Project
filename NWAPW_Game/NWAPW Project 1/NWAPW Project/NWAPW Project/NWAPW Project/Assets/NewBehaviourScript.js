1778b1857ad03999dcafbbcc775e00ca88f7606a
jasongalbraith <jason_galbraith@beaverton.k12.or.us>
2017-07-26 11:45:11 -0700

    HP spritesheet added

---
+++ NWAPW_Game/NWAPW Project 1/NWAPW Project/NWAPW Project/NWAPW Project/Assets/NewBehaviourScript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+
+var speedY : float = 0.05;
+var speedX : float = 0.05;
+
+
+
+function Update () {
+    // Get the horizontal and vertical axis.
+    // By default they are mapped to the arrow keys.
+    // The value is in the range -1 to 1
+    var translationY : float = Input.GetAxis ("Vertical") * speedY;
+    var translationX : float = Input.GetAxis ("Horizontal") * speedX;
+
+    // Make it move 10 meters per second instead of 10 meters per frame...
+    translationY *= Time.deltaTime;
+    translationX *= Time.deltaTime;
+
+	    // Move translation along the object's z-axis
+	    transform.Translate (0, translationY, 0);
+	    // Rotate around our y-axis
+	    transform.Translate (translationX, 0, 0);
+}
+
