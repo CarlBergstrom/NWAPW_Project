563ec739f9d3bd0b6fe564d3192147168fa958cf
jasongalbraith <jason_galbraith@beaverton.k12.or.us>
2017-07-27 12:02:05 -0700

    Movement animations and controls added

---
+++ NWAPW_Game/NWAPW Project 1/Assets/Characters/scripts/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	//Inspector Variables
+	double  playerSpeed = 5.0;
+	//speed player moves
+
+	void Update ()
+	{
+
+		MoveForward (); // Player Movement 
+	}
+
+	void MoveForward ()
+	{
+		Debug.Log("dededed");
+		if (Input.GetKey ("up") || Input.GetKey ("W")) {//Press up arrow key to move forward on the Y AXIS
+			transform.Translate (0,(float)playerSpeed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey ("down") || Input.GetKey ("S")) {//Press up arrow key to move forward on the Y AXIS
+			transform.Translate (0,(float)-playerSpeed * Time.deltaTime, 0);
+		}
+		if (Input.GetKey ("left")|| Input.GetKey ("A")) {//Press up arrow key to move forward on the Y AXIS
+			transform.Translate ((float)-playerSpeed * Time.deltaTime, 0, 0);
+		}
+		if (Input.GetKey ("right")|| Input.GetKey ("D")) {//Press up arrow key to move forward on the Y AXIS
+			transform.Translate ((float)playerSpeed * Time.deltaTime, 0, 0);
+		}
+	}
+}
---
+++ NWAPW_Game/NWAPW Project 1/Assets/Characters/scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float inputX = Input.GetAxisRaw("Horizontal");
+		float inputY = Input.GetAxisRaw("Vertical");
+
+		bool isWalking = (Mathf.Abs(inputX) + Mathf.Abs(inputY)) > 0;
+
+		anim.SetBool ("isWalking", isWalking);
+		if (isWalking)
+		{
+			anim.SetFloat ("X", inputX);
+			anim.SetFloat ("Y", inputY);
+
+			transform.position += new Vector3 (inputX, inputY, 0).normalized * 2 * Time.deltaTime;
+		}
+	}
+}
