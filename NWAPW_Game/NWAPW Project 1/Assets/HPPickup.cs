1db75444066b2415530395cb765718d7fdfd0f2b
jasongalbraith <jason_galbraith@beaverton.k12.or.us>
2017-08-01 14:50:22 -0700

    Heart item now recovers health

---
+++ NWAPW_Game/NWAPW Project 1/Assets/HPPickup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPPickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Collider2D[] pickedUp = Physics2D.OverlapCircleAll (transform.position, 0.5f);
+		if (pickedUp.Length >= 1) {
+			Debug.Log ("Message sent to Player");
+			pickedUp [0].SendMessage ("PlayerRegen1", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
