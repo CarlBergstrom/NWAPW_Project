4b7ae58112b5fe698008a347e71a5079154cf71c
jasongalbraith <jason_galbraith@beaverton.k12.or.us>
2017-08-07 14:06:04 -0700

    Globbor is now fully animated. Can deal damage, cannot yet take damage

---
+++ NWAPW_Game/NWAPW Project 1/Assets/bossHP.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bossHP : MonoBehaviour {
+	Animator anim;
+	
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator>();
+	}
+	
+	void BossDmg()
+	{
+	anim.SetTrigger("hpDown4");
+	}
+	
+}
---
+++ NWAPW_Game/NWAPW Project 1/Assets/spt_pile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spt_pile : MonoBehaviour {
+
+	public int Edamage;
+	public float range;
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		// not biting
+		if(collision.gameObject.layer == 8)
+		{
+			Player.SendMessage("takedamageP", Edamage, SendMessageOptions.DontRequireReceiver);
+		}
+	}
+	
+	void Update ()
+	{
+		Destroy(gameObject, 2f);
+	}
+}
